09a229a9b05cfe95c043d0f24c213b05676e80f8
Satish Kanchanna <sat90210@gmail.com>
2018-02-06 13:41:47 -0500

    Practice cake code

---
+++ src/main/java/com/mysamples/Fibber.java
@@ -0,0 +1,69 @@
+package com.mysamples;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fibber {
+
+	private static Map<Integer, Integer> memo = new HashMap<>();
+
+	public static int fib_recursive(int n) {
+
+		// edge case: negative index
+		if (n < 0) {
+			throw new IllegalArgumentException("Index was negative. No such thing as a negative index in a series");
+		}
+		// base case: 0 or 1
+		else if (n == 0 || n == 1) {
+			return n;
+		}
+		// see if we've already calculated this
+		if (memo.containsKey(n)) {
+			return memo.get(n);
+		}
+
+		int result = fib_recursive(n - 1) + fib_recursive(n - 2);
+
+		// memorize
+		memo.put(n, result);
+
+		return result;
+	}
+
+	public static int fib(int n) {
+
+		// edge cases:
+		if (n < 0) {
+			throw new IllegalArgumentException("Index was negative. No such thing as a negative index in a series.");
+		} else if (n == 0 || n == 1) {
+			return n;
+		}
+		
+		// we'll be building the fibonacci series from the bottom up
+	    // so we'll need to track the previous 2 numbers at each step
+		int prevPrev = 0; 	// 0th fibonacci
+		int prev = 1; 		// 1th fibonacci
+		int current = 0; 	// Declare and initialize current		
+		
+		for (int i = 1; i < n; i++) {
+			
+			// Iteration 1: current = 2nd fibonacci
+	        // Iteration 2: current = 3rd fibonacci
+	        // Iteration 3: current = 4th fibonacci
+	        // To get nth fibonacci ... do n-1 iterations.			
+	        current = prev + prevPrev;
+	        prevPrev = prev;
+	        prev = current;
+		}		
+		
+		return current;
+
+	}
+	
+	public static void main (String[] args) {
+		
+		System.out.println("Fib Count:"+ fib_recursive(6));
+		System.out.println("Fib Count:"+ fib(6));
+	}
+
+}
